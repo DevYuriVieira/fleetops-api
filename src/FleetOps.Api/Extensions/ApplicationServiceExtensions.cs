172c7d089336a0cab20b590f83279ceff8895f2c
Yuri <yurivieirateixeira@hotmail.com>
2026-09-06 01:31:57 -0300

    feat: implement api dependency injection and pipeline

---
+++ src/FleetOps.Api/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,49 @@
+namespace FleetOps.Api.Extensions;
+
+using FleetOps.Application.UseCases.Deliveries;
+using FleetOps.Application.UseCases.Drivers;
+using FleetOps.Application.UseCases.Maintenance;
+using FleetOps.Application.UseCases.Routes;
+using FleetOps.Application.UseCases.Vehicles;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<RegisterVehicleUseCase>();
+        services.AddScoped<ActivateVehicleUseCase>();
+        services.AddScoped<DeactivateVehicleUseCase>();
+        services.AddScoped<AssignDriverToVehicleUseCase>();
+        services.AddScoped<UnassignDriverFromVehicleUseCase>();
+        services.AddScoped<UpdateVehicleMileageUseCase>();
+        services.AddScoped<SendVehicleToMaintenanceUseCase>();
+        services.AddScoped<ReturnVehicleFromMaintenanceUseCase>();
+
+        services.AddScoped<RegisterDriverUseCase>();
+        services.AddScoped<ActivateDriverUseCase>();
+        services.AddScoped<DeactivateDriverUseCase>();
+        services.AddScoped<SuspendDriverUseCase>();
+
+        services.AddScoped<CreateDeliveryUseCase>();
+        services.AddScoped<AssignDeliveryUseCase>();
+        services.AddScoped<StartDeliveryUseCase>();
+        services.AddScoped<CompleteDeliveryUseCase>();
+        services.AddScoped<CancelDeliveryUseCase>();
+
+        services.AddScoped<CreateRouteUseCase>();
+        services.AddScoped<AssignRouteUseCase>();
+        services.AddScoped<AddDeliveryToRouteUseCase>();
+        services.AddScoped<RemoveDeliveryFromRouteUseCase>();
+        services.AddScoped<StartRouteUseCase>();
+        services.AddScoped<CompleteRouteUseCase>();
+        services.AddScoped<CancelRouteUseCase>();
+
+        services.AddScoped<ScheduleMaintenanceUseCase>();
+        services.AddScoped<StartMaintenanceUseCase>();
+        services.AddScoped<CompleteMaintenanceUseCase>();
+        services.AddScoped<CancelMaintenanceUseCase>();
+
+        return services;
+    }
+}
--- src/FleetOps.Api/Program.cs
+++ src/FleetOps.Api/Program.cs
@@ -1,6 +1,26 @@
+using System.Text.Json.Serialization;
+using FleetOps.Api.Extensions;
+using FleetOps.Infrastructure.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplicationUseCases();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
+app.UseStatusCodePages();
+
+app.MapControllers();
 
 app.Run();
 
